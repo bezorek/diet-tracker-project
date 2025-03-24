4532cd51fa0f62e2d4195080e2b1d729596c65d3
Maciej <maciejszymanskki@gmail.com>
2025-03-24 14:33:23 +0100

    Filter products without nutriments

--- src/components/ProductsGrid.tsx
+++ src/components/ProductsGrid.tsx
@@ -3,41 +3,57 @@ import useProducts from "../hooks/useProducts";
 import ProductCard from "./ProductCard";
 import { ProductQuery } from "@/pages/Dashboard";
 import React from "react";
-import InfiniteScroll from 'react-infinite-scroll-component';
+import InfiniteScroll from "react-infinite-scroll-component";
 
-interface Props{
-  productQuery: ProductQuery
+interface Props {
+  productQuery: ProductQuery;
 }
 
-const ProductsGrid = ({productQuery}: Props) => {
-  const {data, error, isLoading, fetchNextPage, hasNextPage} = useProducts(productQuery);
+const ProductsGrid = ({ productQuery }: Props) => {
+  const { data, error, isLoading, fetchNextPage, hasNextPage } =
+    useProducts(productQuery);
 
   if (error) return <Text className="text-red-500">{error.message}</Text>;
 
-  const fetchedGamesCount = data?.pages.reduce((total, page) => total + page.products.length, 0) || 0;
+  const fetchedGamesCount =
+    data?.pages.reduce((total, page) => total + page.products.length, 0) || 0;
   return (
-    <InfiniteScroll dataLength={fetchedGamesCount} hasMore={hasNextPage} next={() => fetchNextPage()} loader={<Spinner />}>
-
-      <SimpleGrid minChildWidth='290px' gap={4}>
+    <InfiniteScroll
+      dataLength={fetchedGamesCount}
+      hasMore={hasNextPage}
+      next={() => fetchNextPage()}
+      loader={<Spinner />}
+    >
+      <SimpleGrid minChildWidth="290px" gap={4}>
         {isLoading && <Spinner></Spinner>}
 
         {data?.pages.map((page, index) => (
           <React.Fragment key={index}>
-            {page.products.map((product) => (
-              <ProductCard
-              key={product.id}
-              name={product.product_name}
-              image_url={product.image_small_url}
-              brand={product.brands}
-              quantity={product.quantity}
-              nutriments={product.nutriments}
-            />
-            ))}
+            {page.products
+              .filter(
+                (product) =>
+                  product.nutriments?.["energy-kcal"] !== undefined &&
+                  product.nutriments?.proteins !== undefined &&
+                  product.nutriments?.fat !== undefined &&
+                  product.nutriments?.carbohydrates !== undefined
+              )
+              .map((product) => {
+                console.log(product);
+                return (
+                  <ProductCard
+                    key={product.id}
+                    name={product.product_name}
+                    image_url={product.image_small_url}
+                    brand={product.brands}
+                    quantity={product.quantity}
+                    nutriments={product.nutriments}
+                  />
+                );
+              })}
           </React.Fragment>
         ))}
       </SimpleGrid>
     </InfiniteScroll>
-
   );
 };
 
